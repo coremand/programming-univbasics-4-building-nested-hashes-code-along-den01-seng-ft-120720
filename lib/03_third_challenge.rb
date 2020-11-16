7a2ec7b85151a272a46bfaf4e7627263d9235b44
Obinna Nwabia <valinevite@gmail.com>
2020-11-16 20:13:00 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,4 +1,5 @@
-epic_tragedy = {
+def third_challenge
+epic_tragedy= {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -13,3 +14,4 @@ epic_tragedy = {
    }
 }
 
+end
